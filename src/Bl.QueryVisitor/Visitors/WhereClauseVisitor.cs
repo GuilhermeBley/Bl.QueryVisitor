a4d41e97cb22891283f54271e6c33bc4d8b45f98
Guilherme Bley <guilhermecbley@gmail.com>
2024-04-01 22:39:23 -0300

    and where clause

--- src/Bl.QueryVisitor/Visitors/WhereClauseVisitor.cs
+++ src/Bl.QueryVisitor/Visitors/WhereClauseVisitor.cs
@@ -1,37 +1,20 @@
 using System.Collections;
 using System.Collections.Immutable;
+using System.IO.Pipes;
 using System.Linq.Expressions;
 
 namespace Bl.QueryVisitor.Visitors;
 
 public class WhereClauseVisitor : ExpressionVisitor
 {
-    private readonly List<InternalClauseInfo> _whereClauses = new List<InternalClauseInfo>();
+    private IEnumerable<WhereClause> _clauseInfos
+        = Enumerable.Empty<WhereClause>();
 
     public IEnumerable<WhereClause> GetWhereClauses(Expression expression)
     {
-        _whereClauses.Clear();
-
-        List<WhereClause> clauses = new();
-        List<ClauseInfo> orClauses = new();
-
         Visit(expression);
 
-        foreach (var clause in _whereClauses)
-        {
-            if (clause.IsNextOrOperator)
-            {
-                orClauses.Add(clause);
-                continue;
-            }
-
-            var whereClause = new WhereClause(orClauses);
-
-            orClauses.Clear();
-            clauses.Add(whereClause);
-        }
-
-        return clauses;
+        return _clauseInfos;
     }
 
     protected override Expression VisitMethodCall(MethodCallExpression node)
@@ -39,67 +22,106 @@ public class WhereClauseVisitor : ExpressionVisitor
         if (node.Method.Name != "Where")
             return base.VisitMethodCall(node);
 
-        if (node.Arguments.Count == 2 && node.Arguments[1] is UnaryExpression unaryExpression)
-        {
-            ExtractBinaryClauses(null!);
-        }
-        
+        var whereVisitor = new InternalBinaryWhereClauseVisitor();
+
+        _clauseInfos = whereVisitor.GetWhereClauses(node);
+
         return base.VisitMethodCall(node);
-    }
-
-    private void ExtractBinaryClauses(BinaryExpression node)
-    {
-        var nextNode = node;
-
-        do
-        {
-            InternalClauseInfo clause;
-
-            switch (nextNode.NodeType)
-            {
-                case ExpressionType.OrElse:
-                    clause = ExtractClauseInfo(nextNode.Left);
-                    clause.IsNextOrOperator = true;
-                    _whereClauses.Add(clause);
-                    break;
-                default:
-                    clause = ExtractClauseInfo(nextNode.Left);
-                    _whereClauses.Add(clause);
-                    break;
-            }
-
-            nextNode = nextNode.Right.CanReduce ?
-                nextNode.Right as BinaryExpression :
-                null;
-
-        } while (nextNode is BinaryExpression);
-    }
-
-    private static InternalClauseInfo ExtractClauseInfo(Expression node)
-    {
-        if (node is not BinaryExpression memberExpression)
-            throw new ArgumentException();
-        
-        var propertyName = memberExpression.Left.ToString();
-        var constant = memberExpression.Right as ConstantExpression;
-        var value = constant?.Value;
-
-        if (constant is null)
-            throw new ArgumentException();
-        
-        var comparer = memberExpression.NodeType.ToString();
-            
-        return new InternalClauseInfo(propertyName, comparer, value);
     }
 
     private record InternalClauseInfo
         : ClauseInfo
     {
-        public bool IsNextOrOperator = false;
+        public bool IsNextAndOperator = false;
 
         public InternalClauseInfo(string PropertyName, string ComparerType, object? Value)
             : base(PropertyName, ComparerType, Value)
         {
+        }
+    }
+
+    private class InternalBinaryWhereClauseVisitor 
+        : ExpressionVisitor
+    {
+        private readonly List<InternalClauseInfo> _whereClauses = new List<InternalClauseInfo>();
+
+        public IEnumerable<WhereClause> GetWhereClauses(Expression expression)
+        {
+            _whereClauses.Clear();
+
+            List<WhereClause> clauses = new();
+            List<ClauseInfo> andClauses = new();
+
+            Visit(expression);
+
+            foreach (var clause in _whereClauses)
+            {
+                if (clause.IsNextAndOperator)
+                {
+                    andClauses.Add(clause);
+                    continue;
+                }
+
+                var whereClause = andClauses.Any() ?
+                    new WhereClause(andClauses) :
+                    new WhereClause(clause);
+
+                andClauses.Clear();
+                clauses.Add(whereClause);
+            }
+
+            return clauses;
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (_whereClauses.Any())
+                return base.VisitBinary(node); // Already collected
+
+            var nextNode = node;
+
+            do
+            {
+                var binaryExp = nextNode.Left as BinaryExpression
+                    ?? nextNode;
+
+                if (binaryExp is null)
+                    break;
+
+                InternalClauseInfo clause;
+
+                switch (nextNode.NodeType)
+                {
+                    case ExpressionType.AndAlso:
+                        clause = ExtractClauseInfo(binaryExp);
+                        clause.IsNextAndOperator = true;
+                        _whereClauses.Add(clause);
+                        break;
+                    default:
+                        clause = ExtractClauseInfo(binaryExp);
+                        _whereClauses.Add(clause);
+                        break;
+                }
+                
+                nextNode = nextNode.Right as BinaryExpression;
+
+            } while (nextNode is BinaryExpression);
+
+            return base.VisitBinary(node);
+        }
+
+        private static InternalClauseInfo ExtractClauseInfo(BinaryExpression node)
+        {
+            var propertyName = node.Left.ToString();
+            var constant = node.Right as ConstantExpression;
+            var value = constant?.Value;
+
+            if (constant is null)
+                throw new ArgumentException();
+
+            var comparer = node.NodeType.ToString();
+
+            return new InternalClauseInfo(propertyName, comparer, value);
         }
     }
 }
@@ -109,8 +131,14 @@ public class WhereClause
 {
     private readonly IReadOnlyList<ClauseInfo> _whereClauses;
     public int Count => _whereClauses.Count;
-    public readonly bool NextIsOrOperator;
+    public readonly bool AndOperator;
     public ClauseInfo this[int index] => _whereClauses[index];
+
+    internal WhereClause(ClauseInfo clause)
+        : this(new[] { clause })
+    {
+
+    }
 
     internal WhereClause(IEnumerable<ClauseInfo> clauses)
     {
@@ -119,7 +147,7 @@ public class WhereClause
         if (_whereClauses.Count == 0)
             throw new IndexOutOfRangeException("It requires at least one item.");
 
-        NextIsOrOperator = _whereClauses.Count > 1;
+        AndOperator = _whereClauses.Count > 1;
     }
 
     public IEnumerator<ClauseInfo> GetEnumerator()
