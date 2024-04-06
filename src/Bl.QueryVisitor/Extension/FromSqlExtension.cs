8a0a0c4a9196a513c3ee392ea48f4f220d563788
Guilherme Bley <guilhermecbley@gmail.com>
2024-04-06 14:31:37 -0300

    Create own queryable

--- src/Bl.QueryVisitor/Extension/FromSqlExtension.cs
+++ src/Bl.QueryVisitor/Extension/FromSqlExtension.cs
@@ -1,15 +1,95 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Collections;
+using System.Linq.Expressions;
 
 namespace Bl.QueryVisitor.Extension;
 
 public static class FromSqlExtension
 {
-    public static IQueryable<TEntity> FromSqlRowE<TEntity>(this DbSet<TEntity> dbSet)
+    public static IQueryable<TEntity> FromSqlRowE<TEntity>(
+        this DbSet<TEntity> dbSet,
+        [NotParameterized] string sql,
+        params object[] parameters)
         where TEntity : class
     {
-        new Pomelo.EntityFrameworkCore.MySql.Query.ExpressionVisitors.Internal.MySqlQuerySqlGenerator(
-            dbSet.EntityType.)
+        // TODO: Throw if contains more than one '{having}'
 
-        return dbSet.FromSqlRaw("",);
+        IQueryable<TEntity>? generatedQueryable = dbSet.FromSqlRaw(sql, parameters);
+        
+        if (!sql.Contains("{having}", StringComparison.OrdinalIgnoreCase))
+            generatedQueryable = new InternalQueryable<TEntity>(generatedQueryable);
+
+        return generatedQueryable;
+    }
+
+    public class InternalQueryable<TEntity>
+        : IQueryable<TEntity>
+    {
+        private readonly IQueryable<TEntity> _efQueryable;
+
+        /// <summary>
+        /// Provider that improves the expressions changes after execution.
+        /// </summary>
+        private readonly InternalQueryProvider _provider;
+
+        public InternalQueryable(IQueryable<TEntity> other)
+        {
+            _efQueryable = other;
+            _provider = new InternalQueryProvider(other.Provider);
+        }
+
+        public Type ElementType => _efQueryable.ElementType;
+
+        public Expression Expression => _efQueryable.Expression;
+
+        public IQueryProvider Provider => _provider;
+
+        public IEnumerator GetEnumerator()
+            => _efQueryable.GetEnumerator();
+
+        IEnumerator<TEntity> IEnumerable<TEntity>.GetEnumerator()
+            => _efQueryable.GetEnumerator();
+    }
+
+    public class InternalQueryProvider 
+        : IQueryProvider,
+        IAsyncQueryProvider
+    {
+        private readonly IQueryProvider _sqlProvider;
+
+        public InternalQueryProvider(IQueryProvider sqlProvider)
+        {
+            _sqlProvider = sqlProvider;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+            => _sqlProvider.CreateQuery(expression);
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+            => _sqlProvider.CreateQuery<TElement>(expression);
+
+        public object? Execute(Expression expression)
+        {
+            return _sqlProvider.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _sqlProvider.Execute<TResult>(expression);
+        }
+
+        public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default)
+        {
+            if (_sqlProvider is not IAsyncQueryProvider asyncQueryProvider)
+                throw new InvalidOperationException("This is not a 'IAsyncQueryProvider'.");
+
+            return asyncQueryProvider.ExecuteAsync<TResult>(expression, cancellationToken);
+        }
+
+        private static Expression NormalizeExpression(Expression expression)
+        {
+            throw new NotImplementedException("Implement having expression visitor.");
+        }
     }
 }
