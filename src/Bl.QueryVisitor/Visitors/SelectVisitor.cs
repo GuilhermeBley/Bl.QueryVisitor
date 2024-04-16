0e3cc544074ecbcd8f57018377ca7c6844ed84d9
Guilherme Bley <guilhermecbley@gmail.com>
2024-04-15 22:53:18 -0300

    Fix select wrong column #5

--- src/Bl.QueryVisitor/Visitors/SelectVisitor.cs
+++ src/Bl.QueryVisitor/Visitors/SelectVisitor.cs
@@ -1,10 +1,16 @@
-﻿using System.Linq.Expressions;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
 
 namespace Bl.QueryVisitor.Visitors;
 
 internal class SelectVisitor : ExpressionVisitor
 {
     private readonly List<string> _columns = new List<string>();
+
+    public SelectVisitor()
+    {
+
+    }
 
     public IEnumerable<string> TranslateColumns(Expression expression)
     {
@@ -15,6 +21,9 @@ internal class SelectVisitor : ExpressionVisitor
 
     protected override Expression VisitMember(MemberExpression node)
     {
+        if (Type.GetTypeCode(node.Type) == TypeCode.Object)
+            return base.VisitMember(node);
+
         _columns.Add(node.Member.Name);
         return base.VisitMember(node);
     }
