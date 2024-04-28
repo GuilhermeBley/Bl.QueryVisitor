f7083a28946fe839d49f96a2417fb33e31ac0d5c
Guilherme Bley <guilhermecbley@gmail.com>
2024-04-27 23:14:17 -0300

    Add common methods #8

--- src/Bl.QueryVisitor/Visitors/MethodParamVisitor.cs
+++ src/Bl.QueryVisitor/Visitors/MethodParamVisitor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using System.Text;
 
 namespace Bl.QueryVisitor.Visitors;
 
@@ -6,7 +7,7 @@ internal class MethodParamVisitor
     : ExpressionVisitor
 {
     private readonly ParamDictionary _parameters;
-
+    private StringBuilder _builder = new();
     public IReadOnlyDictionary<string, object?> Parameters => _parameters;
 
     public MethodParamVisitor(ParamDictionary parameters)
@@ -14,5 +15,12 @@ internal class MethodParamVisitor
         _parameters = parameters;
     }
 
-    
+    public string TranslateMethod(Expression? expression)
+    {
+        _builder.Clear();
+
+        Visit(expression);
+
+        return _builder.ToString();
+    }
 }
